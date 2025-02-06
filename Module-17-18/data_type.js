8f82404fd90d42fde64e49027e69ad4266d745fc
Risan <borhansiddque19@gmail.com>
2025-02-06 12:46:00 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Data type - String, Number, Boolean

---
+++ Module-17-18/data_type.js
@@ -0,0 +1,8 @@
+// ========== STRING DATA TYPE ========
+let fullName = "Borhan Siddque";
+
+// ========== NUMBER DATA TYPE ============
+let age = 50;
+
+// ========= BOOLEAN DATA TYPE -- True/False ==========
+let isLoggedIn = false;
