89f38c25627969bd42e34ddae9dde88c3a3855df
Risan <borhansiddque19@gmail.com>
2025-02-06 12:52:57 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Simple IF / ELSE

---
+++ Module-17-18/free_drinks.js
@@ -0,0 +1,12 @@
+const buyBurger = 499;
+
+// Simple IF / ELSE
+if (buyBurger >= 500) {
+    console.log("You Got a Free Coke");
+    
+} else {
+    console.log("You have no Free Coke");
+}
+
+// Ternary operation
+buyBurger >= 500 ? console.log("You Got a Free Coke") : console.log("You have no Free Coke");
