16a04975f8d81d98f29cd8bb573c7f3f3990566a
Risan <borhansiddque19@gmail.com>
2025-02-06 19:05:27 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Sumit Saha JS Comparisons

--- Module-17-18/Sumit - Javascript Comparisons/comparison_operator.js
+++ Module-17-18/Sumit - Javascript Comparisons/comparison_operator.js
@@ -28,3 +28,50 @@
 
 
 // =========== LOGICAL OPERATORS ===========
+
+const age = 20;
+// &&
+
+// if (age >= 18 && age <= 20 && age == 18) {
+//     console.log("You Adult");
+    
+// } else {
+//     console.log("Go outside");
+// }
+
+
+// ||
+
+// if (age >= 18 || age <= 20 || age == 18) {
+//     console.log("You Adult");
+    
+// } else {
+//     console.log("Go outside");
+// }
+
+
+//=============== Ternary Operator
+
+// if (age >= 18) {
+//     console.log("You are Adult!");
+// } else {
+//     console.log("You are not Adult!");
+// }
+
+// age >= 18 ? console.log("You are Adult!") : console.log("You are not Adult!");
+// console.log(age >= 18 ? "You are Adult!" : "You are not Adult!");
+
+
+
+console.log(2 < 12);
+console.log(2 < "12");
+console.log(2 < "John");
+console.log(2 > "John");
+console.log("2" > "12");
+console.log("2" < "12");
+console.log("2" == "12");
+
+
+
+
+
