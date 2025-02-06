d27395c500a0b058209462c67141212ff69d1237
Risan <borhansiddque19@gmail.com>
2025-02-06 15:55:32 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Sumit Saha JS Comparisons

---
+++ Module-17-18/Sumit - Javascript Comparisons/comparison_operator.js
@@ -0,0 +1,30 @@
+// =========== COMPARISONS OPERATORS ===========
+// let x = 6;
+// let y = "6";
+
+// console.log(x == y);
+// console.log(x === y);
+
+
+// console.log(x != 6);
+
+// console.log(x !== 6);
+// console.log(x !== y);
+// console.log(x !== 8);
+
+
+// console.log(x > 8);
+// console.log(x < 8);
+// console.log(x >= 8);
+// console.log(x <= 8);
+
+
+// let age = 11;
+
+// if (age >= 18) {
+//     console.log('You are Adult!');
+// }
+
+
+
+// =========== LOGICAL OPERATORS ===========
