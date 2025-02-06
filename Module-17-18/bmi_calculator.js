7987c11b69e6bd51baf9b5ea72addb017a4b1874
Risan <borhansiddque19@gmail.com>
2025-02-06 13:07:24 +0600

    Milestone 4 Practice - module 17 & 18 Practice - BMI Calculator - IF / ELSE IF / ELSE

---
+++ Module-17-18/bmi_calculator.js
@@ -0,0 +1,11 @@
+const BMI = 19;
+
+if (BMI < 18.5) {
+    console.log("Underweight");
+} else if (BMI <= 24.9) {
+    console.log("Normal Weight");
+} else if (BMI <= 29.9) {
+    console.log("Overweight");
+} else {
+    console.log("Obese");
+}
