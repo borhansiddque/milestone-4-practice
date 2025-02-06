1586bb131c758dca9383f57a1c6c7cfb1364a58f
Risan <borhansiddque19@gmail.com>
2025-02-06 12:41:37 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Variable

---
+++ Module-17-18/variable.js
@@ -0,0 +1,19 @@
+// ======== VAR Keyword =============
+var age = 10; // Valid
+var age = 15; // Valid
+age = 8; // Valid
+
+
+// ============= LET Keyword =============
+let money = 500; // Valid
+// let money = 1000; Invalid
+money = 300; // Valid
+
+
+// =========== CONST Keyword =============
+const weight = 60; // Valid
+// const weight = 70; // Invalid
+// weight = 65; // Invalid
+
+console.log(weight);
+
