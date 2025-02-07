b90f08c3ed48aa82a2a2efaff9785118da6bfbc4
Risan <borhansiddque19@gmail.com>
2025-02-08 01:58:26 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Shradha Khapra - Array practice

---
+++ Module-17-18/Shradha Khapra/qs_3.js
@@ -0,0 +1,24 @@
+/**
+ * QS -> Create an array to store companies -> "Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"
+ * A. Remove the first company from the array.
+ * B. Remove Uber & Add Apple in its place.
+ * C. Add Amazon at the end. 
+*/
+
+// Question Solved
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+console.log(companies);
+
+// A
+// companies.shift();
+// console.log(companies);
+
+// B
+// companies.splice(2, 1, "Apple");
+// console.log(companies);
+
+// C
+// companies.push("Amazon");
+// console.log(companies);
+ 
