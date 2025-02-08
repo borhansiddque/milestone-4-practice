13b2f5a40bc22e7346ddc52ceb421b5529ab9a4f
Risan <borhansiddque19@gmail.com>
2025-02-08 20:02:16 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Array practice

---
+++ 18.5 Arrays/arr_task_two.js
@@ -0,0 +1,25 @@
+/** 
+ * 2. Add or remove elements
+        2.1 Declare an array of 3 tourist destinations
+        2.2 Add a new tourist destination to your tourist array
+        2.3 Add two more to your array
+        2.4 Remove the last tourist destination you have added
+        2.5 display the final array as output
+*/
+
+// 2.1
+let tourPlaces = ["Istanbul", "Goa", "Barcelona"];
+// console.log(tourPlaces);
+
+
+// 2.2
+tourPlaces.push("Bali")
+// console.log(tourPlaces);
+
+// 2.3
+tourPlaces.push("Varkala", "Los Angeles");
+// console.log(tourPlaces);
+
+// 2.4
+tourPlaces.pop();
+console.log(tourPlaces);
