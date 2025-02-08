7e0beba3e59fe9124e277a43289794eaf03cd106
Risan <borhansiddque19@gmail.com>
2025-02-08 22:41:32 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Array practice

---
+++ 18.5 Arrays/arr_task_four.js
@@ -0,0 +1,13 @@
+/** 
+ * 4. Checking if it's an Array
+        Instructions:
+        4.1 Create different variables, each containing either an array or a non-array value.
+        4.2 Now use isArray to check if each variable is an array.
+        4.3 Print a message to the console indicating whether each variable is an array or not.
+*/
+
+let fullName = "Borhan Siddque";
+let marks = [50, 60, 70, 80];
+
+console.log(Array.isArray(fullName));
+console.log(Array.isArray(marks));
