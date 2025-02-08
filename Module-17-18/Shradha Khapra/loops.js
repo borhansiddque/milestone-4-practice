de8eba323712c1830e0cdb7fe09dbdbb30cc3cf8
Risan <borhansiddque19@gmail.com>
2025-02-08 19:53:46 +0600

    Milestone 4 Practice - module 17 & 18 Practice - Array practice

---
+++ Module-17-18/Shradha Khapra/loops.js
@@ -0,0 +1,4 @@
+// let sum = "Risan";
+for (let i = 0; i<= 1; i++) {
+    console.log("Risan");
+}
---
+++ Module-17-18/arr_task_one.js
@@ -0,0 +1,21 @@
+/** 
+ * 1. Declare an array
+        1.1 Declare an array with 5 elements containing fruits
+        1.2 console log the 3rd index element
+        1.3 change the value of the 2nd index element to jambura
+        1.4 console log the final array
+*/
+
+// 1.1
+let fruits = ["Apple", "Orange", "Banana", "Water Melon", "Papaya"];
+
+// 1.2
+console.log(fruits[3]);
+
+// 1.3
+fruits.splice(2, 1, "Jambura");
+
+// 1.4
+console.log(fruits);
+
+
