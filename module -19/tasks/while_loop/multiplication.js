b77ffb8f45f839c8ffc89dd90ec545aa25ee47f9
Risan <borhansiddque19@gmail.com>
2025-02-09 19:56:12 +0600

    Milestone 4 Practice - module 19 - Loop Practice

--- module -19/tasks/while_loop/multiplication.js
+++ module -19/tasks/while_loop/multiplication.js
@@ -1,4 +1,11 @@
 /***
-    Generate a multiplication table for number 9
+    Generate a multiplication table for number 5
 */
 
+let number = 5;
+let multiNumber = 1;
+
+while (multiNumber <= 10) {
+    console.log(`${number} * ${multiNumber} = ${number * multiNumber}`);
+    multiNumber++;
+}
