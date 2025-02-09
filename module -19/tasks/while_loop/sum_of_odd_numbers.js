2e9c7345c57464d270a5c43abf4d7c8a77bfba0c
Risan <borhansiddque19@gmail.com>
2025-02-09 19:25:26 +0600

    Milestone 4 Practice - module 19 - Loop Practice

--- module -19/tasks/while_loop/sum_of_odd_numbers.js
+++ module -19/tasks/while_loop/sum_of_odd_numbers.js
@@ -4,13 +4,36 @@
 
 */
 
+// let oddNumStart = 91;
+// let sum = 0;
 
+// while (oddNumStart <= 129) {
+//     console.log(oddNumStart);
+//     sum += oddNumStart;
+//     console.log("Sum: ", sum);
+//     oddNumStart += 2;
+    
+// }
+
+// console.log("Total Odd Numbers Sum: ", sum);
 
 
 
 /***
     Subtask-2:
-
     Display sum of all the even numbers from 51 to 85.
 */
 
+let evenNumber = 206;
+let sum = 0;
+
+while (evenNumber <= 311) {
+    if (evenNumber % 2 === 0) {
+        console.log("EvenNumber: ", evenNumber);
+        sum += evenNumber;
+        console.log("Sum: ", sum);
+    }
+    evenNumber++;
+}
+
+console.log("206 to 311 all even numbers sum: ", sum);
