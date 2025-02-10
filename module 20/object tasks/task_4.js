d20238fdeff072d81feaadb7b3cd5c1eec4323f9
Risan <borhansiddque19@gmail.com>
2025-02-11 00:45:20 +0600

    Milestone 4 Practice - module 20 - Practice Object

---
+++ module 20/object tasks/task_4.js
@@ -0,0 +1,24 @@
+// Count the number of properties.
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+let propertyCount = Object.keys(student).length;
+console.log(propertyCount);
+
+
+
+const cat = {
+    name: 'Meaow',
+    age: '4 Year',
+    color: 'White',
+    isPregnant: false,
+    isFriendly: true,
+};
+
+let objPropertyCount = Object.keys(cat).length;
+console.log(objPropertyCount);
