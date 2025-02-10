f47c8f2089db5590a13e5075010a35f7c0f830e7
Risan <borhansiddque19@gmail.com>
2025-02-11 00:35:42 +0600

    Milestone 4 Practice - module 20 - Practice Object

---
+++ module 20/object tasks/task_3.js
@@ -0,0 +1,12 @@
+// Display the physics marks as output.
+const student = {
+    name: "Borhan Siddque",
+    id: 5421,
+    accounting: {
+        subject: "HSC Accounting",
+        author: "Mosharf karim",
+        marks: 30
+    }
+};
+
+console.log(student.accounting.marks);
