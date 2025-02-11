a2e976da26fe8edd1adb85e96ebdbb94d1f2469c
Risan <borhansiddque19@gmail.com>
2025-02-11 19:38:32 +0600

    Milestone 4 Practice - module 20 - Practice

---
+++ module 20/test.js
@@ -0,0 +1,8 @@
+const userText = "BorhanSiddque19@gmail.Com";
+const text = "borhansiddque19@gmail.com";
+
+if (userText.trim().toLowerCase() === text) {
+    console.log("You Can Sign in - Valid email");
+} else {
+    console.log('Please Stay out');
+}
