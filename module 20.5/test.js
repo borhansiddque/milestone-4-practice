2561f8e329f44342c05c05956f99ef76af7caf7d
Risan <borhansiddque19@gmail.com>
2025-02-14 00:31:07 +0600

    Add function to identify and log even and odd numbers from an array

---
+++ module 20.5/test.js
@@ -0,0 +1,21 @@
+// const numbers = [80, 63, 19, 41, 20, 50, 70, 88, 75, 99, 11];
+// for (i = 0; i < numbers.length; i++) {
+//     const arrNumber = numbers[i];
+//     if (arrNumber % 2 === 0) {
+//         console.log(arrNumber);
+//     } else {
+//         console.log("Odd number");
+//     }
+// }
+
+const numbers2 = [80, 63, 19, 41, 20, 9, 4, 5];
+for (i = 0; i < numbers2.length; i++){
+    const findNumber = numbers2[i];
+    if (findNumber % 2 === 0) {
+        console.log("Even number", findNumber);
+    } else {
+        console.log("Odd number", findNumber);
+    }
+};
+
+
